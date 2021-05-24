d02baee1110f67b7b49f9b0cb33c0af5ed34998e
JiHyeonMon <jhk8806@naver.com>
2021-05-24 17:07:55 +0900

    회원 더미 데이터 삭제 & 블로그 상세 글 불러오기 구현

--- routes/blog.js
+++ routes/blog.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Blog = require('../models/blog');
 
+
 // 모든 포스터들 조회
 router.get('/', function(req, res, next) {
     Blog.find({}).then(all => res.json({allPosts:all}));
@@ -28,5 +29,27 @@ router.post('/create', function(req, res, next) {
         }
     );
 });
+
+// 블로그 글 상세 페이지를 위한 코드
+router.get('/:id', function(req, res, next) {
+    const id = req.params.id;
+
+    Blog.findById(id)
+    .then(data => {
+        if (!data) {
+            res.status(404).send({ message: "Not found blog with id " + id });
+        } else {
+            res.json({detailPost: data});
+        }
+    })
+    .catch(err => {
+        res
+        .status(500)
+        .send({ message: "Error retrieving blog with id=" + id });
+    });
+
+
+});
+
 
 module.exports = router;
